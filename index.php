7c0680e5d29ca32a9fafefde583d6c0c6b9d8fe1
massa <massafranck@gmail.com>
2018-07-31 15:25:56 +0200

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice6</title>
+</head>
+<body>
+  <p>
+    <?php
+    //Teste que les paramètres de l'url existent
+    if(isset($_GET['building']) && isset($_GET['room'])){
+      echo $_GET['building'] . ' ' . $_GET['room'];
+    }
+    ?>
+  </p>
+</body>
+</html>
