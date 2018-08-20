76fb28c48cffaf2185f13cc756a612d250861c13
massa <massafranck@gmail.com>
2018-08-20 22:26:47 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice6</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Teste que les paramètres de l'url existent et affichage des valeurs
-    if(isset($_GET['building']) && isset($_GET['room'])){
-      echo $_GET['building'] . ' ' . $_GET['room'];
-    }
-    ?>
-  </p>
+    <head>
+        <meta charset="utf-8" />
+        <title>exercice6</title>
+    </head>
+    <body>
+        <?php
+        if (!empty($_GET['building']) && !empty($_GET['room'])) {
+            // affichage des paramètres
+            echo $_GET['building'] . ' ' . $_GET['room'];
+        }
+        ?>
+    </p>
 </body>
 </html>
